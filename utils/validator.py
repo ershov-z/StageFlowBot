773ddce662a0bfa3dcc98ca6c91ed38df1579b3e
ershov-z <zahar.ershov1@gmail.com>
2025-11-05 20:43:55 +0300

    force push all changes

--- utils/validator.py
+++ utils/validator.py
@@ -3,7 +3,7 @@
 import copy
 import random
 from loguru import logger
-from main import send_message  # ✅ используется существующая функция для отправки сообщений в Telegram
+from bot.main import send_message  # ✅ используется существующая функция для отправки сообщений в Telegram
 
 
 # ============================================================
